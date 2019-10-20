ff9cabfc97cc0b8fd913106fecde4a58d5f1d7c8
honvedp <honvedp@gmail.com>
2019-10-20 10:06:16 +0200

    test data

---
+++ data/eventsData.js
@@ -0,0 +1,28 @@
+	var employersD = [
+						{id: 'en_1', name: 'Erdélyi Árpád', email: 'erdelyi.arpad@nextent.hu'},
+						{id: 'en_2', name: 'Honvéd Péter', email: 'honved.peter@nextent.hu'},
+						{id: 'en_3', name: 'Jenei Krisztián', email: 'jenei.krisztian@nextent.hu'},
+						{id: 'en_4', name: 'Kollerné Koska Veronika', email: 'koska.veronika@nextent.hu'},
+						{id: 'en_5', name: 'László József', email: 'laszlo.jozsef@nextent.hu'},
+						{id: 'en_6', name: 'Pernye László', email: 'pernye.laszlo@nextent.hu'},
+						{id: 'en_7', name: 'Radnai László', email: 'radnai.laszlo@nextent.hu'},
+						{id: 'en_8', name: 'Sipos Annamária', email: 'sipos.annamaria@nextent.hu'},
+						{id: 'en_9', name: 'Kovács Ákos', email: 'kovacs.akos@nextent.hu'}
+						
+	]
+	var projektsD = [
+						{code: 'pn_MT_DWH_DEV_SUP_TM_2019', name: 'MT Adattárház fejlesztés és üzemeltetés T&M 2019', managerID: 'en_2'},
+						{code: 'pn_ERSTE_2019', name: 'ERSTE tesztelés 2019', managerID: 'en_9'},
+	]
+
+	var conversationsD = [
+                        {id: 'ce_123',  eventDT: '2019-10-10T12:00:00', userIdFrom: 'en_1', userIdTo: 'en_2', messageType: 'email', messageID: '16b69682844ed1a5'},
+                        {id: 'ce_124',  eventDT: '2019-10-10T12:05:00', userIdFrom: 'en_2', userIdTo: 'en_1', messageType: 'email', messageID: '16b69682844ed1a5'},
+                        {id: 'ce_125',  eventDT: '2019-10-12T13:00:00', userIdFrom: 'en_8', userIdTo: 'en_4', messageType: 'email', messageID: '16b69682844ed1a5'},
+/*                        {eventDT: '2017-11-08T00:00:00', userGroup: '', userName: 'USER_12', objectOwner: '', objectName: 'OBJECT_20'},                                                                             
+                        {eventDT: '2017-11-08T00:00:00', userGroup: '', userName: 'USER_10', objectOwner: '', objectName: 'OBJECT_319'},                                                                            
+                        {eventDT: '2017-11-08T00:00:00', userGroup: '', userName: 'USER_17', objectOwner: '', objectName: 'OBJECT_4'},                                                                              
+                        {eventDT: '2017-11-08T23:50:00', userGroup: '', userName: 'USER_6', objectOwner: '', objectName: 'OBJECT_294'},                                                                             
+                        {eventDT: '2017-11-08T23:50:00', userGroup: '', userName: 'USER_41', objectOwner: '', objectName: 'OBJECT_236'},                                                                            
+*/						
+]					
